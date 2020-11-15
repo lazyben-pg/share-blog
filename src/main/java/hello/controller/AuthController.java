f0299abb0d0fbc41a9d20ceff64a2f9e34c0ec9c
lazyben <458861463@qq.com>
2020-11-15 14:50:26 +0800

    完成用户鉴权逻辑

--- src/main/java/hello/controller/AuthController.java
+++ src/main/java/hello/controller/AuthController.java
@@ -1,4 +1,102 @@
 package hello.controller;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.inject.Inject;
+import java.time.Instant;
+import java.util.Map;
+
+import hello.entity.User;
+
+@Controller
 public class AuthController {
+    private final UserDetailsService userDetailsService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthController(UserDetailsService userDetailsService, AuthenticationManager authenticationManager) {
+        this.userDetailsService = userDetailsService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @GetMapping("/auth")
+    @ResponseBody
+    public Object auth() { return new Result("ok", false); }
+    @PostMapping("/auth/login")
+    @ResponseBody
+    public Result login(@RequestBody Map<String, String> usernameAndPassword) {
+        String username = usernameAndPassword.get("username");
+        String password = usernameAndPassword.get("password");
+
+        UserDetails userDetails = null;
+        try {
+            userDetails = userDetailsService.loadUserByUsername(username);
+        } catch (UsernameNotFoundException e) {
+            return new Result("fail", "用户不存在");
+        }
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password, userDetails.getAuthorities());
+        try {
+            authenticationManager.authenticate(token);
+            SecurityContextHolder.getContext().setAuthentication(token);
+            return new Result("ok", "登陆成功", new User(1, "hunger", "null", Instant.now(), Instant.now()));
+        } catch (BadCredentialsException e) {
+            return new Result("fail", "密码不正确");
+        }
+    }
+
+    private static class Result {
+        private final String status;
+        private final String msg;
+        private final User data;
+        private final boolean login;
+
+        private Result(String status, String msg, User data, Boolean login) {
+            this.status = status;
+            this.msg = msg;
+            this.data = data;
+            this.login = login;
+        }
+
+        public Result(String status, String msg) {
+            this(status, msg, null);
+        }
+
+        public Result(String status, String msg, User data) {
+            this(status, msg, data, false);
+        }
+
+        public Result(String status, Boolean login, User data) {
+            this(status, null, data, login);
+        }
+
+        public Result(String status, Boolean login) {
+            this(status, null, null, login);
+        }
+
+        public String getMsg() {
+            return msg;
+        }
+
+        public User getData() {
+            return data;
+        }
+
+        public boolean isLogin() {
+            return login;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+    }
 }
--- src/main/java/hello/entity/User.java
+++ src/main/java/hello/entity/User.java
@@ -1,22 +1,39 @@
 package hello.entity;
 
+import java.time.Instant;
+
 public class User {
     private int id;
-    private String name;
+    private String username;
+    private String avatar;
+    private Instant updateAt;
+    private Instant createdAt;
+
+    public User(int id, String username, String avatar, Instant updateAt, Instant createdAt) {
+        this.id = id;
+        this.username = username;
+        this.avatar = avatar;
+        this.updateAt = updateAt;
+        this.createdAt = createdAt;
+    }
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public String getUsername() {
+        return username;
     }
 
-    public String getName() {
-        return name;
+    public String getAvatar() {
+        return avatar;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public Instant getUpdateAt() {
+        return updateAt;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
     }
 }
